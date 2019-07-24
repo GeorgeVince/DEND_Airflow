9851b1a7be8b8ee49d2b7cbc4ca43d9d439e3ea2
George Vince <George.Vince@hotmail.co.uk>
2019-07-24 09:22:58 +0100

    bug: remove dot in dag names
    Having a DOT in dag names causes them to not render correctly in
    the airflow UI.

--- dags/lesson_1_exercise_2.py
+++ dags/lesson_1_exercise_2.py
@@ -15,8 +15,8 @@ def hello_world():
 # TODO: Add a daily `schedule_interval` argument to the following DAG
 #
 dag = DAG(
-        "lesson1.exercise2",
-        start_date=datetime.datetime.now() - datetime.timedelta(days=2)
+        "lesson1_exercise2",
+        start_date=datetime.datetime.now() - datetime.timedelta(days=2),
         schedule_interval="@daily")
 
 task = PythonOperator(
